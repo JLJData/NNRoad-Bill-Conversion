7bb4dad9fa2959a5453e9e25e27f612c96a835de
hailey.chen-uGUQl <hailey.cheng@jljdata.com>
2026-07-30 17:34:28 +0800

    UK

--- profiles/hk_vertical_l/__init__.py
+++
@@ -1,2 +0,0 @@
-# -*- coding: utf-8 -*-
-"""旧目录名兼容；正式引擎包为 profiles.hk_payroll_calc。"""
--- profiles/hk_vertical_l/convert.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-"""旧包名兼容：请改用 profiles.hk_payroll_calc。"""
-from profiles.hk_payroll_calc.convert import *  # noqa: F401,F403
